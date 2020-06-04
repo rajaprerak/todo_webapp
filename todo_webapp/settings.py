d8811218062525d8a1cf67d7160a6f065ac50556
Prerak Raja <rajaprerak@gmail.com>
2020-06-04 17:36:55 +0530

    project added

--- todo_webapp/settings.py
+++ todo_webapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%=$h62coe(2%hqmrq)z)92*gei(-eo0a)l1vr)x9v2f1zio#$j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todo-awesome-app.herokuapp.com','127.0.0.1']
 
 
 # Application definition
